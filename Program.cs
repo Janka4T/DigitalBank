69c7684ab5939a495bb21bf7df8e8778141c0eb0
Jānis Tulišs <janistuliss3@gmail.com>
2020-05-29 16:31:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DigitalBag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
